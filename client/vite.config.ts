2c93df8c2ee7b7bfa561652dbdd69ff91f70ee80
kobayashim <63332389+kobayashi12m@users.noreply.github.com>
2025-08-14 16:10:54 +0900

    vite設定変更

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // これでネットワークURLが表示される
+    open: true  // 起動時に自動でブラウザを開く
+  }
 })
