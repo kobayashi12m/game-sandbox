48b6886f207f1a9828e394c5d061bf2cbed2df6e
kobayashim <63332389+kobayashi12m@users.noreply.github.com>
2026-01-11 18:35:07 +0900

    voconsole表示を.envで設定できるようにする

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+// vConsole（モバイルデバッグ用）
+if (import.meta.env.VITE_VCONSOLE_ENABLED === "true") {
+  import("vconsole").then((module) => {
+    new module.default();
+  });
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
